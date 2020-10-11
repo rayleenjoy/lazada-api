96d9aa61d1cbfacf64a6691f7db472a35a5927cd
rayleenjoy <rayleenjoy.eulogio@gmail.com>
2020-10-11 14:37:31 +0800

    update lazadaapi class

--- src/LazadaAPI.php
+++ src/LazadaAPI.php
@@ -8,15 +8,46 @@ use rayleenj\lazadaapi\lazada_sdk\LazopRequest;
 
 class LazadaAPI {
 
+	protected $apiLink = '';
+	protected $apiKey = '';
+	protected $apiSecret = '';
+	protected $apiAccessToken = '';
+	protected $lazopClient = '';
+
 	public function __construct(array $api = [])
 	{
-		$apiLink = ($api['apiLink']) ??  '';
-		$apiKey = ($api['apiKey']) ??  '';
-		$apiSecret = ($api['apiSecret']) ??  '';
-		$this->apiAccessToken = ($api['apiAccessToken']) ??  '';
-		$this->lazopclient =  new LazopClient($apiLink, $apiKey, $apiSecret);
+		$checkValid = $this->checkValid($api);
+		if(!empty($checkValid)){
+			throw new Exception($checkValid,0);
+		}
+		$this->apiLink = $api['apiLink'];
+		$this->apiKey = $api['apiKey'];
+		$this->apiSecret = $api['apiSecret'];
+		$this->apiAccessToken = $api['apiAccessToken'];
+		$this->lazopclient =  $this->lazopclient();
 		return $this->lazopclient;
 	} 
+
+	public function checkValid($api)
+	{
+		if(!isset($api['apiLink']) || empty($api['apiLink'])){
+			return 'apiLink not defined or empty. ';
+		}
+		if(!isset($api['apiKey']) || empty($api['apiKey'])){
+			return  'apiKey not defined or empty. ';
+		}
+		if(!isset($api['apiSecret']) || empty($api['apiSecret'])){
+			return 'apiSecret not defined or empty.';
+		}
+		if(!isset($api['apiAccessToken']) || empty($api['apiAccessToken'])){
+			return  'apiAccessToken not defined or empty.';
+		}
+		return '';
+	}
+
+	public function lazopclient(){
+		return new LazopClient($this->apiLink, $this->apiKey, $this->apiSecret);
+	}
 
 	public function api_connect($api_name, $httpMethod = 'POST',$parameters = []){
 		$request = new LazopRequest($api_name, $httpMethod);
@@ -26,8 +57,7 @@ class LazadaAPI {
 			}
 		}
 		$result = $this->lazopclient->execute($request, $this->apiAccessToken);
-		$data = json_decode($result);
-		return $data;
+		return json_decode($result);
 	}
 
 }
